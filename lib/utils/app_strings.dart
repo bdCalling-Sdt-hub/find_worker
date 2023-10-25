8942dc863192dd9d045314f81c85c4b1dbb9ca40
jubayed004 <mohammadjubayed.islam97@gmail.com>
2023-10-25 16:37:37 +0600

    first commit

---
+++ lib/utils/app_strings.dart
@@ -0,0 +1,150 @@
+class AppStrings{
+
+  static const String logo = "Logo";
+  static const String welcomeToFindWorker = "Welcome to Find Worker!";
+  static const String loginAs = "Login As";
+  static const String user = "User";
+  static const String serviceProvider = "Service Provider";
+  static const String didNotHaveAnAccount = "Didn’t have an account?";
+  static const String signUp = "Sign Up";
+  static const String orContinueWith = "Or continue with";
+  static const String forgetPassword = "Forget Password";
+  static const String enterYourPassword = "Enter your password";
+  static const String password = "Password";
+  static const String enterYourEmail = "Enter your email";
+  static const String email = "Email";
+
+  static const String fullName = "Full Name";
+  static const String enterYourFullName = "Enter your full name";
+
+  static const String dateOfBirth = "Date of Birth";
+
+  static const String dd = "DD";
+  static const String mm = "MM";
+  static const String yyyy = "YYYY";
+  static const String gender = "Gender";
+  static const String male = "Male";
+  static const String female = "Female";
+  static const String others = "Others";
+  static const String phoneNumber = "Phone Number";
+  static const String address = "Address";
+  static const String enterYourAddress = "Enter your address";
+  static const String serviceType = "Service Type";
+  static const String enterYourService = "Enter your service";
+  static const String enterDigitPassword = "Enter 8-10 digit password";
+  static const String confirmPassword = "Confirm Password";
+
+  static const String alreadyHaveAnAccount = "Already have an account?";
+  static const String homeClean = "Home Clean";
+  static const String carWash = "Car Wash";
+  static const String farmer = "Farmer";
+  static const String airConditionMaintenance = "Air Condition Maintenance";
+  static const String pipeFitter = "Pipe Fitter";
+  static const String jensSalon = "Jens Salon";
+  static const String manDriver = "Man Driver";
+  static const String womanDriver = "Woman Driver";
+  static const String ladiesSalon = "Ladies Salon";
+  static const String homeBusiness = "Home Business";
+  static const String butcher = "Butcher";
+  static const String privateTutor = "Private Tutor";
+  static const String henna = "THenna";
+  static const String movers = "Movers";
+  static const String gypsumBoardFloor = "Gypsum Board & Floor";
+  static const String carTiresRepair = "Car Tires Repair";
+  static const String carRecovery = "Car Recovery";
+  static const String catering = "Catering";
+  static const String cableFixing = "Cable Fixing";
+  static const String enterEmailAddressToResetPassword = "Enter email address to reset password.";
+
+  static const String getOTP = "Get OTP";
+  static const String oTP = "OTP";
+  static const String pleaseEnterTheOTPCode = "Please enter the OTP code.";
+
+  static const String didNotGetTheCode = "Did not get the code?";
+  static const String resend = "Resend";
+  static const String verify = "Verify";
+  static const String resetPassword = "Reset Password ";
+  static const String passwordMustHaveCharacters = "Password must have 8-10 characters.";
+
+  static const String online = "Online";
+  static const String service = "Service";
+  static const String status = "Status";
+  static const String time = "Time";
+  static const String date = "Date";
+  static const String noJobsPending = "No jobs pending";
+  static const String home ="Home";
+  static const String location = "Location";
+  static const String reject ="Reject";
+  static const String accept = "Accept";
+
+  static const String notification = "Notification";
+  static const String welcomeToTheFindWorker = "Welcome to the Find Worker.";
+
+  static const String profile= "Profile";
+  static const String personalInformation= "Personal Information";
+  static const String serviceDetails= "Service Details";
+  static const String settings = "Settings";
+  static const String  logout= "Logout";
+  static const String  editPersonalInformation= "Edit Personal Information";
+  static const String  update= "Update";
+  static const String  doYouWantToLogoutYourProfile = "Do you want to logout your profile?";
+  static const String  yes= "Yes";
+  
+  static const String  no= "No";
+  static const String  description= "Description";
+  static const String  loremIpsumDolor= "Lorem ipsum dolor sit amet consectetur. Faucibus platea eget fringilla orci amet eget nisl porttitor quam. Tempus nulla amet dignissim nunc ante feugiat tristique. Ipsum sed odio augue id sed blandit auctor porta ut. Varius etiam nisl diam tellus nullam integer tincidunt. Tempus viverra lacus nunc tristique nulla.";
+  static const String  edit= "Edit";
+  static const String  editServiceDetails= "Edit Service Details";
+  static const String  addNewService= "Add New Service";
+  static const String  enterServiceDescription= "Enter service description";
+  static const String  addService= "Add Service";
+  
+  static const String jobDetails = "Job Details";
+  static const String complete = "Complete";
+  static const String clickOnTheCompleteButton = "Click on the “Complete” button after finishing your current job to complete the job status.";
+  static const String congratulations = "Congratulations!";
+  static const String youHaveCompletedTheJob = "You have completed the job.";
+  static const String goToHome = "Go to Home";
+  static const String language = "Language";
+  static const String changePassword = "Change Password";
+  static const String termsOfServices = "Terms of Services";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String aboutUs = "About us";
+  static const String  support = "Support";
+
+  static const String   reEnterYourNewPassword = "Re-enter your new password";
+  static const String   enterYourEmailAddressToVerifyOTP = "Enter your E-mail address to verify OTP";
+  static const String   save = "Save";
+
+  static const String   weSentaVerification = "We sent a verification code to your email. Please";
+  static const String   jobHistory = "Job History";
+  static const String   recentJobs = "Recent Jobs";
+  static const String   noJobsHereYet = "No jobs here yet.";
+  static const String   doYouWantToRemoveThisJobDetails = "Do you want to remove this job details?";
+  static const String   searchByCategories = "Search by categories" ;
+  static const String   apply= "Apply";
+  static const String   filter= "Filter";
+  static const String   category= "Category";
+  static const String   seeAll= "See All";
+  static const String   pending= "Pending";
+  static const String   aboutJob= "About Job";
+  static const String   hireDetails= "Hire Details";
+  static const String   hireList= "Hire List";
+  static const String rateUs = "Rate Us";
+  static const String keepPatienceWaitingForApproval = "Keep patience. Waiting for approval.";
+  static const String cancel = "Cancel";
+  static const String or ="Or";
+  static const String directCall = "Direct Call";
+  
+  static const String oopsSomethingWentWrong ="Oops...Something went wrong. ";
+  static const String tryAgain ="Try Again";
+  static const String approved ="Approved!";
+  static const String call ="Call";
+  static const String giveRatingOutOf ="Give rating out of 5!";
+  static const String writeYourFeedback ="Write your feedback";
+  static const String submit ="Submit";
+  static const String didNotSeeWhat  ="Didn’t see what you’re looking for?Go to Categories " ;
+
+
+
+}
