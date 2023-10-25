799c4fc3ebae826ddaa574b21a00a0cbd75e1aee
Nadimpg <bsmrstunadimcse@gmail.com>
2023-10-25 16:33:50 +0600

    fix(static image icons & colors,custom widgets,routing): complete

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,51 @@
+import 'dart:ui';
+
+class AppColors{
+  static const Color blue_100 = Color(0xff0668E3);
+  static const Color blue_80 = Color(0xff3886E9);
+  static const Color blue_60 = Color(0xff6AA4EE);
+  static const Color blue_40 = Color(0xff9BC3F4);
+  static const Color blue_20 = Color(0xffCDE1F9);
+  static const Color blue_10 = Color(0xffE7F0FD);
+  static const Color blue_5 = Color(0xffF2F7FD);
+
+  static const Color red_100 = Color(0xffD7263D);
+  static const Color red_80 = Color(0xffDD4F62);
+  static const Color red_60 = Color(0xffE37987);
+  static const Color red_40 = Color(0xffE9A2AB);
+  static const Color red_20 = Color(0xffEFCCD0);
+  static const Color red_10 = Color(0xffF2E1E3);
+  static const Color red_5 = Color(0xffF4EBEC);
+
+  static const Color green_100 = Color(0xff2BA24C);
+  static const Color green_80 = Color(0xff52B26D);
+  static const Color green_60 = Color(0xff7AC18E);
+  static const Color green_40 = Color(0xffA0D0AD);
+  static const Color green_20 = Color(0xffC8DFCE);
+  static const Color green_10 = Color(0xffDCE8E0);
+  static const Color green_5 = Color(0xffE5EBE7);
+
+
+  static const Color yellow_100 = Color(0xffFFB701);
+  static const Color yellow_80 = Color(0xffFFC534);
+  static const Color yellow_60 = Color(0xffFFD467);
+  static const Color yellow_40 = Color(0xffFFE299);
+  static const Color yellow_20 = Color(0xffFFF1CC);
+  static const Color yellow_10 = Color(0xffFFF8E6);
+  static const Color yellow_5 = Color(0xffFFFBF2);
+
+
+  static const Color black_100 = Color(0xff333333);
+  static const Color black_80 = Color(0xff5A5A5A);
+  static const Color black_60 = Color(0xff818181);
+  static const Color black_40 = Color(0xffA7A7A7);
+  static const Color black_20 = Color(0xffCECECE);
+  static const Color black_10 = Color(0xffE2E2E2);
+  static const Color black_5 = Color(0xffECECEC);
+
+  static const Color white = Color(0xffFFFFFF);
+  static const Color whiteBg = Color(0xffCCE0FA);
+
+
+
+}
