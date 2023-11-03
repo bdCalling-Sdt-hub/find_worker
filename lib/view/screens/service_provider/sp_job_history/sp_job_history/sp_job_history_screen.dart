83c4b7601ef28424ad60f3c6b34f3b335b8336fb
Nadimpg <bsmrstunadimcse@gmail.com>
2023-11-03 17:38:06 +0600

    feat(all section): incomplete

--- lib/view/screens/service_provider/sp_job_history/sp_job_history/sp_job_history_screen.dart
+++ lib/view/screens/service_provider/sp_job_history/sp_job_history/sp_job_history_screen.dart
@@ -1,13 +1,10 @@
 import 'package:find_worker/core/app_routes.dart';
 import 'package:find_worker/utils/app_colors.dart';
 import 'package:find_worker/utils/app_icons.dart';
-import 'package:find_worker/utils/app_images.dart';
-import 'package:find_worker/utils/app_strings.dart';
 import 'package:find_worker/view/widgets/image/custom_image.dart';
 import 'package:find_worker/view/widgets/text/custom_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class SpJobHistoryScreen extends StatefulWidget {
   const SpJobHistoryScreen({super.key});
@@ -17,6 +14,7 @@ class SpJobHistoryScreen extends StatefulWidget {
 }
 
 class _SpJobHistoryScreenState extends State<SpJobHistoryScreen> {
+  int selected=2;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -62,73 +60,81 @@ class _SpJobHistoryScreenState extends State<SpJobHistoryScreen> {
                     Get.toNamed(AppRoute.spHistoryDetailsScreen);
                   },
                   child: Column(
-                    children: List.generate(20, (index) => Container(
+                    children: List.generate(20, (index) =>
+                        GestureDetector(
+                          onTap: (){
+                            setState(() {
+
+                            });
+                          },
+                          child: Container(
                       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 14),
-                      decoration: const ShapeDecoration(
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(width: 1, color: Color(0xFFE7F0FD)),
-                        ),
+                      decoration:  ShapeDecoration(
+                          color:index % 2 == 0 ? AppColors.black_5 : AppColors.white,
+                          shape: RoundedRectangleBorder(
+                            side: BorderSide(width: 1, color: Color(0xFFE7F0FD)),
+                          ),
                       ),
                       child: Row(
                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          const CustomText(
-                            text: 'Smith John',
-                            fontSize: 12,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          const Row(
-                            children: [
-                           Row(
-                             children: [
-                               CustomImage(
-                                   imageSrc: AppIcons.calendar
-                               ),
-                               CustomText(
-                                 left: 4,
-                                 text:  '12-Sept',
-                                 fontSize: 10,
-                                 fontWeight: FontWeight.w400,
-                                 right: 4,
-                               ),
-                             ],
-                           ),
-                             SizedBox(width: 8,),
+                          children: [
+                            const CustomText(
+                              text: 'Smith John',
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            const Row(
+                              children: [
                              Row(
                                children: [
                                  CustomImage(
-                                     imageSrc: AppIcons.clock,
-                                   size: 12,
+                                     imageSrc: AppIcons.calendar
                                  ),
                                  CustomText(
                                    left: 4,
-                                   text: '12:00 AM',
-                                   textAlign: TextAlign.right,
+                                   text:  '12-Sept',
                                    fontSize: 10,
                                    fontWeight: FontWeight.w400,
                                    right: 4,
                                  ),
                                ],
-                             )
-                            ],
-                          ),
-                          Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
-                            decoration: ShapeDecoration(
-                              color: AppColors.green_10,
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                             ),
+                               SizedBox(width: 8,),
+                               Row(
+                                 children: [
+                                   CustomImage(
+                                       imageSrc: AppIcons.clock,
+                                     size: 12,
+                                   ),
+                                   CustomText(
+                                     left: 4,
+                                     text: '12:00 AM',
+                                     textAlign: TextAlign.right,
+                                     fontSize: 10,
+                                     fontWeight: FontWeight.w400,
+                                     right: 4,
+                                   ),
+                                 ],
+                               )
+                              ],
                             ),
-                            child: const CustomText(
-                              color: AppColors.green_100,
-                              text:  'Complete',
-                              fontSize: 12,
-                              fontWeight: FontWeight.w400,
+                            Container(
+                              padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+                              decoration: ShapeDecoration(
+                                color: AppColors.green_10,
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                              ),
+                              child: const CustomText(
+                                color: AppColors.green_100,
+                                text:  'Complete',
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
                       ),
-                    ),),
+                    ),
+                        ),),
                   ),
                 ),
               ],
