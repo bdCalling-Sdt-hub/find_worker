dad8dcd47a1edccc9570a9a0b573cacc4722836d
palash-barman <palash900202@gmail.com>
2023-11-09 17:10:10 +0600

    complete Service Provider History

--- lib/view/screens/service_provider/sp_job_history/sp_job_history/sp_job_history_screen.dart
+++ lib/view/screens/service_provider/sp_job_history/sp_job_history/sp_job_history_screen.dart
@@ -21,9 +21,7 @@ class SpJobHistoryScreen extends StatefulWidget {
 }
 
 class _SpJobHistoryScreenState extends State<SpJobHistoryScreen> {
-    final _controller = Get.put(SpHistoryController());
-
-
+  final _controller = Get.put(SpHistoryController());
 
   @override
   Widget build(BuildContext context) {
@@ -35,136 +33,187 @@ class _SpJobHistoryScreenState extends State<SpJobHistoryScreen> {
         appBar: AppBar(
           automaticallyImplyLeading: false,
           title: const CustomText(
-            color: AppColors.blue_100,
-             fontWeight: FontWeight.w500,
+              color: AppColors.blue_100,
+              fontWeight: FontWeight.w500,
               fontSize: 18,
-              text: AppStrings.jobHistory
-          ),
+              text: AppStrings.jobHistory),
           centerTitle: true,
         ),
-        body: LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
-          return  Obx(()=> _controller.loading.value?const CustomLoader():_controller.historyList.isEmpty?Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(AppImages.empty,height: 80,width: 80,),
-                const Text("No Job History yet.")
-              ],
-            ),
-          )
-            : RefreshIndicator(
-              onRefresh: () async{
-                _controller.getHistoryList(false);
-              },
-              child: SingleChildScrollView(
-                physics: const AlwaysScrollableScrollPhysics(),
-                padding: const EdgeInsetsDirectional.symmetric(horizontal: 20,vertical: 24),
-                child: Column(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      decoration:const BoxDecoration(
-                        color: AppColors.blue_100,
-                        borderRadius: BorderRadius.only(topLeft: Radius.circular(8),topRight: Radius.circular(8)),
-                      ),
-                      padding: const EdgeInsets.only(left: 12,top: 16,bottom: 16),
-
-                      child:const CustomText(
-                        color: AppColors.white,
-                        textAlign: TextAlign.start,
-                        text: "Recent Jobs",
-                        fontWeight: FontWeight.w500,
-                        fontSize: 18,
-                      ),
-                    ),
-                    Column(
-                      children: List.generate(_controller.historyList.length, (index) {
-                        final demoData=_controller.historyList[index];
-                        return GestureDetector(
-                          onTap: (){
-                            Get.to(SpHistoryDetailsScreen(historyModel: demoData, index:index,));
-                          },
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 14),
-                            decoration: const ShapeDecoration(
-                              color: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                side: BorderSide(width: 1, color: Color(0xFFE7F0FD)),
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return Obx(
+              () => _controller.loading.value
+                  ? const CustomLoader()
+                  : _controller.historyList.isEmpty
+                      ? Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                AppImages.empty,
+                                height: 80,
+                                width: 80,
                               ),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      CustomText(
-                                        text:_controller.historyList[index].name!,
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                      Column(
-                                        children: [
-                                          Row(
-                                            children: [
-                                              const CustomImage(
-                                                  imageSrc: AppIcons.calendar
-                                              ),
-                                              CustomText(
-                                                  left: 4,
-                                                  text: DateFormat('dd MMM').format(demoData.createAt!),
-                                                  fontSize: 10,
-                                                  fontWeight: FontWeight.w400,
-                                              ),
-                                            ],
-                                          ),
-                                          Row(
-                                            children: [
-                                              const CustomImage(
-                                                  imageSrc: AppIcons.clock
-                                              ),
-                                              CustomText(
-                                                left: 4,
-                                                text: DateFormat.jm().format(demoData.createAt!),
-                                                textAlign: TextAlign.right,
-                                                fontSize: 10,
-                                                fontWeight: FontWeight.w400,
-                                              ),
-                                            ],
-                                          )
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Container(
-                                    padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
-                                    decoration: ShapeDecoration(
-                                      color: AppColors.green_10,
-                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-                                    ),
-                                    child:  CustomText(
-                                      color: AppColors.green_100,
-                                      text:demoData.status!,
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
+                              const Text("No Job History yet.")
+                            ],
                           ),
-                        );
-                      },),
-                    ),
-                  ],
-                )
-              ),
-            ),
-          );
-        },),
+                        )
+                      : RefreshIndicator(
+                          onRefresh: () async {
+                            _controller.getHistoryList(false);
+                          },
+                          child: SingleChildScrollView(
+                              physics: const AlwaysScrollableScrollPhysics(),
+                              padding: const EdgeInsetsDirectional.symmetric(
+                                  horizontal: 20, vertical: 24),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    width: MediaQuery.of(context).size.width,
+                                    decoration: const BoxDecoration(
+                                      color: AppColors.blue_100,
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(8),
+                                          topRight: Radius.circular(8)),
+                                    ),
+                                    padding: const EdgeInsets.only(
+                                        left: 12, top: 16, bottom: 16),
+                                    child: const CustomText(
+                                      color: AppColors.white,
+                                      textAlign: TextAlign.start,
+                                      text: "Recent Jobs",
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  Column(
+                                    children: List.generate(
+                                      _controller.historyList.length,
+                                      (index) {
+                                        final demoData =
+                                            _controller.historyList[index];
+                                        return GestureDetector(
+                                          onTap: () {
+                                            Get.to(SpHistoryDetailsScreen(
+                                              historyModel: demoData,
+                                              index: index,
+                                            ));
+                                          },
+                                          child: Container(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 8, vertical: 14),
+                                            decoration: const ShapeDecoration(
+                                              color: Colors.white,
+                                              shape: RoundedRectangleBorder(
+                                                side: BorderSide(
+                                                    width: 1,
+                                                    color: Color(0xFFE7F0FD)),
+                                              ),
+                                            ),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment
+                                                      .spaceBetween,
+                                              children: [
+                                                Expanded(
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      CustomText(
+                                                        text: _controller
+                                                            .historyList[index]
+                                                            .name!,
+                                                        fontSize: 12,
+                                                        fontWeight:
+                                                            FontWeight.w500,
+                                                      ),
+                                                      Column(
+                                                        children: [
+                                                          Row(
+                                                            children: [
+                                                              const CustomImage(
+                                                                  imageSrc: AppIcons
+                                                                      .calendar),
+                                                              CustomText(
+                                                                left: 4,
+                                                                text: DateFormat(
+                                                                        'dd MMM')
+                                                                    .format(demoData
+                                                                        .createAt!),
+                                                                fontSize: 10,
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .w400,
+                                                              ),
+                                                            ],
+                                                          ),
+                                                          Row(
+                                                            children: [
+                                                              const CustomImage(
+                                                                  imageSrc:
+                                                                      AppIcons
+                                                                          .clock),
+                                                              CustomText(
+                                                                left: 4,
+                                                                text: DateFormat
+                                                                        .jm()
+                                                                    .format(demoData
+                                                                        .createAt!),
+                                                                textAlign:
+                                                                    TextAlign
+                                                                        .right,
+                                                                fontSize: 10,
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .w400,
+                                                              ),
+                                                            ],
+                                                          )
+                                                        ],
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                                Expanded(
+                                                  child: Container(
+                                                    padding: const EdgeInsets
+                                                        .symmetric(
+                                                        horizontal: 4,
+                                                        vertical: 4),
+                                                    decoration: ShapeDecoration(
+                                                      color: AppColors.green_10,
+                                                      shape:
+                                                          RoundedRectangleBorder(
+                                                              borderRadius:
+                                                                  BorderRadius
+                                                                      .circular(
+                                                                          4)),
+                                                    ),
+                                                    child: CustomText(
+                                                      color:
+                                                          AppColors.green_100,
+                                                      text: demoData.status!,
+                                                      fontSize: 12,
+                                                      fontWeight:
+                                                          FontWeight.w400,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              )),
+                        ),
+            );
+          },
+        ),
       ),
     );
   }
