843105536ec0cb8638ba922f3027a5917b70a899
rafsanopi <rafsan3@dti.ac>
2024-01-04 15:58:30 +0600

    Fix

--- lib/view/screens/splash/splash_screen.dart
+++ lib/view/screens/splash/splash_screen.dart
@@ -5,12 +5,10 @@ import 'package:wrcontacts/core/share_pre.dart';
 import 'package:wrcontacts/helper/SystemChromeHelper/system_chrome.dart';
 import 'package:wrcontacts/utils/app_constents.dart';
 import 'package:wrcontacts/utils/app_icons.dart';
-import 'package:wrcontacts/utils/app_strings.dart';
 import 'package:wrcontacts/view/screens/service_provider/sp_bottom_nav_bar/sp_bottom_nav_bar_screen.dart';
 import 'package:wrcontacts/view/screens/user/user_bottom_nav_bar/user_bottom_nav_bar_screen.dart';
-import 'package:wrcontacts/view/widgets/text/custom_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 import 'package:get/get.dart';
 
 import '../service_provider/Binding/provider_bottombar_binding.dart';
@@ -22,22 +20,26 @@ class SplashScreen extends StatefulWidget {
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
+
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     SystemChromeHelper.disableSystemChrome();
-    Timer(const Duration(seconds: 2), ()async{
-      var logged= await PrefsHelper.getString(AppConstants.logged);
-      if(logged==AppConstants.userType){
-        Get.off(UserBottomNavBarScreen(currentIndex: 0),binding:UserBottomNavBinding());
-      }else if(logged==AppConstants.serviceProviderType){
-        Get.off(SpBottomNavBarScreen(currentIndex: 0),binding:ProviderBottomNavBinding());
-      }else{
+    Timer(const Duration(seconds: 2), () async {
+      var logged = await PrefsHelper.getString(AppConstants.logged);
+      if (logged == AppConstants.userType) {
+        Get.off(UserBottomNavBarScreen(currentIndex: 0),
+            binding: UserBottomNavBinding());
+      } else if (logged == AppConstants.serviceProviderType) {
+        Get.off(SpBottomNavBarScreen(currentIndex: 0),
+            binding: ProviderBottomNavBinding());
+      } else {
         Get.offAndToNamed(AppRoute.onboardScreen);
       }
     });
     super.initState();
   }
+
   // @override
   // void dispose() {
   //   // TODO: implement dispose
@@ -46,27 +48,35 @@ class _SplashScreenState extends State<SplashScreen> {
   // }
   @override
   Widget build(BuildContext context) {
-    return  SafeArea(
+    return SafeArea(
         top: false,
         bottom: false,
         child: Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width:MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFFF3F3F3),Color(0xFFCCE0FA),],
+          body: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFFF3F3F3),
+                  Color(0xFFCCE0FA),
+                ],
+              ),
+            ),
+            child: Center(
+              child: Image.asset(
+                AppIcons.logoImage,
+                height: 94,
+                width: 60,
+                fit: BoxFit.fill,
+              ),
+              // child:CustomText(
+              //   text: AppStrings.logo,
+              //   fontSize: 16,
+              //   fontWeight: FontWeight.w600,
+              // ),
+            ),
           ),
-        ),
-        child:  Center(
-          child: Image.asset(AppIcons.logoImage,height:94,width: 60,fit: BoxFit.fill,),
-          // child:CustomText(
-          //   text: AppStrings.logo,
-          //   fontSize: 16,
-          //   fontWeight: FontWeight.w600,
-          // ),
-        ),
-      ),
-    ));
+        ));
   }
 }
